b912d4ec77df637f3046ee72768aa2be940f21c1
sahilmangla01 <manglasahil9050@gmail.com>
2023-10-09 17:33:04 +0530

    first

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require('mongodb')
 
-const uri="mongodb+srv://manglasahil9050:Drumsofliberation@cluster0.k69emc1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+const uri=process.env.Mongo-uri 
 
 const client = new MongoClient(uri, {useNewUrlParser:true})
 
